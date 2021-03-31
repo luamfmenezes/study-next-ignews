7da43ec3e130d20faa37976490e118793ac39cab
Luam F Menezes <luamfmenezes@gmail.com>
2021-03-30 21:46:29 -0300

    feat: integrate faunaDb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export const SignInButton: React.FC = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button onClick={() => signOut()} className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      SignIn with Github
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button onClick={() => signIn("github")} className={styles.signInButton}>
       <FaGithub color="#eba417" />
       SignIn with Github
     </button>
